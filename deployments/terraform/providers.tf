2476e53982f2468fbdaf332164ed83da9e702512
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-03-23 08:05:17 +0530

    added changes

--- deployments/terraform/providers.tf
+++ deployments/terraform/providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "ltbackendstorage"
+    resource_group_name  = "sample"
     storage_account_name = "ltbackendstorage"
     container_name       = "tfstate"
     key                  = "springpetclinic.tfstate"
